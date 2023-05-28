12b9a18ef3a938953d35a1810bd67e29dd33afd2
Denis Kraev <54892326+DeimosDomino@users.noreply.github.com>
2023-05-28 23:16:43 +0300

    Create task9.py

---
+++ autochecks/task9.py
@@ -0,0 +1,136 @@
+import subprocess, sys, re
+
+imports_to_add = '''
+#include <iostream>
+#include <chrono>
+#include <cstdlib> 
+#include <thread>
+#include <atomic>
+#include <vector>
+#include <mutex>
+
+
+'''
+
+code_to_add = '''
+template<typename T>
+void ThreadsafeStack<T>::push(const T &value){
+    std::shared_ptr<Node<T>> newNode = std::make_shared<Node<T>>(value);
+    
+    std::shared_ptr<Node<T>> first = this->front;
+    this->front = newNode;
+    this->front->next = first;
+    return;
+};
+
+
+template<typename T>
+int ThreadsafeStack<T>::length(){
+    std::shared_ptr<Node<T>> cur = this->front;
+    int length = 0;
+    while(cur != nullptr){
+        cur = cur->next;
+        length++;
+    }
+    return length;
+}
+
+
+
+int main(){
+    ThreadsafeStack<int> queue{};
+    srand((unsigned)time(NULL));
+
+    for(int i=0;i<400;i++){
+        queue.push(i);
+    }
+    
+    int count = 4; 
+
+    std::vector<std::thread*> threads;
+
+    int rightLength = 400;
+    for(int i = 0;i<count;i++){
+        int popCount = rand() % 100;
+        rightLength -= popCount;
+        
+        threads.push_back(new std::thread(
+                [&queue, popCount](){  
+                    for(int i = 0;i < popCount;i++){
+                        queue.pop();
+                    }
+                }   
+            )
+        );
+    };
+
+    for(int i = 0;i < count;i++) threads[i]->join();
+    for(int i = 0;i < count;i++) delete threads[i];
+
+
+    std::string result = "Wrong asnwer";
+
+    if(queue.length() == rightLength) result = "OK";
+    std::cout << result << std::endl;
+    return 0;
+}
+
+'''
+
+
+
+def generate():
+    return ["OK\n"]
+
+def solve(dataset):
+    return 'test'
+
+def check(reply, clue):
+    return true
+
+# Write the student code to a file prog.cpp
+student_answer = """{{ STUDENT_ANSWER | e('py') }}"""
+
+functions_to_add = '''
+public:
+    int length();
+    void push(const T &value);
+'''
+
+splitted = re.split(r'ThreadsafeStack\s*{', student_answer)
+student_answer = splitted[0] + 'ThreadsafeStack {' + functions_to_add + splitted[1]
+
+with open("prog.cpp", "w") as src:
+    print(imports_to_add, file=src)
+    print(student_answer, file=src)
+    print('\n', file=src) 
+    print(code_to_add, file=src)
+
+# tests = generate()
+# len_tests = len(tests)
+correct_count = 0
+output = ''
+correct_output = ''
+return_code = subprocess.check_call("g++ -std=c++14 -pthread prog.cpp -o prog".split())
+if return_code != 0:
+    print("** Compilation failed. Testing aborted **", file=sys.stderr)
+if return_code == 0:
+    try:
+        output = subprocess.check_output(["./prog"], universal_newlines=True).strip()
+        correct_output = """{{TEST.expected}}""".strip()
+    except subprocess.CalledProcessError as e:
+        if e.returncode > 0:
+            # Ignore non-zero positive return codes
+            if e.output:
+                print(e.output)
+        else:
+            # But negative return codes are signals - abort
+            if e.output:
+                print(e.output, file=sys.stderr)
+            if e.returncode < 0:
+                print("Task failed with signal", -e.returncode, file=sys.stderr)
+            print("** Further testing aborted **", file=sys.stderr)
+if output == correct_output:
+    print(correct_output)
+else:
+    print(output)
